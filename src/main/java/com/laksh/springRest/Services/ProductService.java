2b71b88fcfa3373ea40100bc98123f66807ef500
laksh-mehra09 <lakshmehra2609@gmail.com>
2026-07-14 10:32:41 +0530

    Modified the methods

--- src/main/java/com/laksh/springRest/Services/ProductService.java
+++ src/main/java/com/laksh/springRest/Services/ProductService.java
@@ -1,6 +1,8 @@
 package com.laksh.springRest.Services;
 
 import com.laksh.springRest.Product.Product;
+import com.laksh.springRest.Repository.ProductRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -10,47 +12,26 @@ import java.util.List;
 @Service
 public class ProductService {
 
-    List<Product> products = new ArrayList<>(Arrays.asList(
-            new Product(101, "Headphone", 1500),
-            new Product(102, "Laptop", 80000)
-    ));
+    @Autowired
+    ProductRepo repo;
 
     public List<Product> getProducts(){
-        return products;
+        return repo.findAll();
     }
 
     public Product getProduct(int id){
-
-        for(Product p : products){
-            if(p.getId() == id){
-                return p;
-            }
-        }
-        return null;
+        return repo.findById(id).get();
     }
 
     public Product addProduct(Product p){
-        products.add(p);
-        return p;
+        return repo.save(p);
     }
 
-    public void updateProduct(Product p){
-
-        int index = 0;
-        for(int i=0; i<products.size(); i++)
-            if(products.get(i).getId() == p.getId())
-                index = i;
-
-        products.set(index, p);
+    public Product updateProduct(Product p){
+        return repo.save(p);
     }
 
     public void deleteProduct(int id){
-
-        int index = 0;
-        for(int i=0; i<products.size(); i++)
-            if(products.get(i).getId() == id)
-                index = i;
-
-        products.remove(index);
+        repo.deleteById(id);
     }
 }
